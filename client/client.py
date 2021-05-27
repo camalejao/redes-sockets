46b50fec596e6136431571be7a424e8ab8656d71
camalejao <jvfsl@ic.ufal.br>
2021-05-27 13:17:49 -0300

    comentando codigo

--- client/client.py
+++ client/client.py
@@ -30,29 +30,36 @@ def parse_args():
     
     return ip, port
 
+# Função para listar os arquivos disponíveis no servidor
 def list_files(s):
     s.send(f"ls{SEPARATOR}".encode())
     res = s.recv(BUFFER_SIZE)
     print(res.decode('utf-8'))
-    
+
+# Função para enviar arquivos do cliente ao servidor
 def send_file(s):
-    filepath = input("Olá! Informe o caminho absoluto do arquivo que será enviado (ou apenas seu nome, caso esteja no mesmo diretório. Obs: não esqueça a extensão do arquivo):\n")
-    filename = input("Informe com qual nome esse arquivo será salvo no servidor: ")
+    # Recebemos a entrada do cliente
+    filepath = input("Informe o caminho absoluto do arquivo que será enviado (ou apenas seu nome, caso esteja no mesmo diretório. Obs: não esqueça a extensão do arquivo):\n")
+    filename = input("\nInforme com qual nome esse arquivo será salvo no servidor: ")
 
     filesize = 0
 
+    # verificamos se o arquivo existe
     try:
         filesize = os.path.getsize(filepath)
     except FileNotFoundError as e:
-        print("Não conseguimos encontrar o arquivo informado")
+        print("\nErro: Não conseguimos encontrar o arquivo informado\n\n")
         return
 
+    # Comunicamos ao servidor que enviaremos um novo arquivo
     s.send(f"new{SEPARATOR}{filename}{SEPARATOR}{filesize}".encode())
 
+    # Aguardamos a confirmação do servidor
     res = s.recv(BUFFER_SIZE)
     if res.decode('utf8') == 'clear':
         print("Enviando...")
 
+    # Abrimos o arquivo e a medida que lemos seus bytes, os enviamos pelo socket
     sent = 0
     with open(filepath, "rb") as f:
         while True:
@@ -65,23 +72,35 @@ def send_file(s):
             sent += len(bytes_read)
             print(100*sent/filesize, "%")
     
+    # Aguardamos a confirmação do servidor que o arquivo foi recebido
     res = s.recv(BUFFER_SIZE)
     print(res.decode('utf-8'))
 
+
+# Função para realizar download de arquivos localizados no servidor
 def download_files(s):
+    # Primeiramente listamos os arquivos disponíveis
     list_files(s)
 
+    # Recebemos a entrada do usuário com o nome do arquivo desejado
     filename = input("Informe o nome do arquivo: ")
 
+    # Enviamos a requisição para o arquivo
     s.send(f"get{SEPARATOR}{filename}".encode())
 
+    # Recebemos do servidor os detalhes do arquivo
     received = s.recv(BUFFER_SIZE).decode()
-
-    filename, filesize = received.split(SEPARATOR)
+    try:
+        filename, filesize = received.split(SEPARATOR)
+    except ValueError:
+        print(received)
+        print("O arquivo requisitado não existe no servidor\n\n")
+        return
 
     filesize = int(filesize)
     remaining = filesize
 
+    # Caso ja exista arquivo com esse nome, é adicionado um prefixo indicando o número da cópia
     aux = filename
     i = 1
     while os.path.isfile(aux):
@@ -89,8 +108,10 @@ def download_files(s):
         i += 1
     filename = aux
 
+    # Sinalizamos para o servidor realizar o envio do arquivo
     s.send("clear".encode())
 
+    # Abrimos o arquivo e escrevemos o bytes a medida que eles são lidos pelo socket
     with open(filename, "wb") as f:
         while True:
             if remaining <= 0:
@@ -101,16 +122,20 @@ def download_files(s):
 
             remaining -= len(bytes_read)
             print(100*(filesize - remaining)/filesize, "%")
-    msg = "Sucesso: arquivo recebido"
+    
+    # Ao término da escrita, sinalizamos o sucesso da operação ao servidor e ao usuário
+    msg = "Sucesso: arquivo recebido\n\n"
     s.send(str.encode(msg))
     print(msg)
 
         
     
 def main():
+    # Criamos o socket do lado do cliente
     s = socket.socket()
     s.connect((HOST, PORT))
 
+    # Menu de opções para as funcionalidades do programa
     opt = 999
     while opt != 0:
         print("Opções:")
@@ -127,14 +152,19 @@ def main():
         elif opt == 3:
             download_files(s)
         elif opt == 0:
+            # Para sair, comunicamos o encerramento da execução do cliente
             s.sendall(f"stop{SEPARATOR}".encode())
+    # Antes de encerrar, fechamos o socket aberto
     s.close()
 
-
+# Leitura dos argumentos passados para o programa
 ip, port = parse_args()
-SEPARATOR = "###"
+
+# Constantes de configuração do cliente
+SEPARATOR = "###" # separador utilizado nas mensagens do protocolo criado
 BUFFER_SIZE = 4096
 HOST = ip or "127.0.0.1"
 PORT = port or 5050
 
+# Início do programa
 main()
--- server/server.py
+++ server/server.py
@@ -29,8 +29,9 @@ def parse_args():
     return port
 
 
-
+# Função para receber arquivo do cliente
 def receive_file(received, client_socket):
+    # Recuperamos as informações sobre o arquivo presentes na mensagem do cliente
     cmd, filename, filesize = received.split(SEPARATOR)
         
     filesize = int(filesize)
@@ -38,6 +39,7 @@ def receive_file(received, client_socket):
     
     filepath = os.path.join(os.getcwd(), "files", filename)
     
+    # Caso um arquivo de mesmo nome já exista, adicionamos um prefixo indicando o numero da cópia
     aux = filepath
     i = 1
     while os.path.isfile(aux):
@@ -45,8 +47,10 @@ def receive_file(received, client_socket):
         i += 1
     filepath = aux
 
+    # Sinalizamos que o cliente está autorizado a enviar os bytes do arquivo
     client_socket.send("clear".encode())
     
+    # A medida que lemos os bytes chegando ao socket, escrevemos no arquivo
     with open(filepath, "wb") as f:
         while True:
             if remaining <= 0:
@@ -57,11 +61,13 @@ def receive_file(received, client_socket):
 
             remaining -= len(bytes_read)
             print(100*(filesize - remaining)/filesize, "%")
+    # Por fim, comunicamos o sucesso da operação
     client_socket.sendall(str.encode("Sucesso: arquivo enviado"))
 
 
+# Função para listar os arquivos disponíveis
 def list_files(client_socket):
-    print("list files")
+    # Os arquivos disponíveis são os localizados no diretório server/files
     files = os.listdir(os.path.join(os.getcwd(), "files"))
     response = "Arquivos disponíveis:\n"
     for f in files:
@@ -69,7 +75,9 @@ def list_files(client_socket):
     client_socket.sendall(str.encode(response))
 
 
+# Função para enviar arquivos do servidor para o cliente
 def send_files(received, client_socket):
+    # recuperamos a mensagem que requisita um arquivo
     msg = received.split(SEPARATOR)
     
     filename = msg[1]
@@ -77,18 +85,22 @@ def send_files(received, client_socket):
 
     filesize = 0
 
+    # Verificamos se o arquivo realmente existe
     try:
         filesize = os.path.getsize(filepath)
     except FileNotFoundError as e:
         client_socket.send(str.encode("Erro: arquivo não encontrado"))
         return
 
+    # Comunicamos as informações sobre o arquivo
     client_socket.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
+    # Aguardamos o cliente sinalizar que podemos enviar os bytes do arquivo
     res = client_socket.recv(BUFFER_SIZE)
     if res.decode('utf8') == 'clear':
         print("Enviando...")
-        
+
+    # Abrimos o arquivo e a medida que lemos seus bytes, enviamos pelo socket
     sent = 0
     with open(filepath, "rb") as f:
         while True:
@@ -101,11 +113,14 @@ def send_files(received, client_socket):
             sent += len(bytes_read)
             print(100*sent/filesize, "%")
     
+    # Por fim, aguardamos a confirmação de sucesso da operação pelo cliente.
     res = client_socket.recv(BUFFER_SIZE)
     print(res.decode('utf-8'))
 
 
+# Função para lidar com cada cliente conectado
 def handle_client(client_socket, address):
+    # Mantemos a conexão ativa até o cliente comunicar o encerramento
     while True:
 
         try:
@@ -116,8 +131,9 @@ def handle_client(client_socket, address):
         
         # Separando os dados recebidos para interpretar mensagem
         msg = received.split(SEPARATOR)
-
         cmd = msg[0]
+
+        # Menu para tratar comandos recebidos
         if cmd == "new":
             receive_file(received, client_socket)
         elif cmd == "ls":
@@ -125,6 +141,7 @@ def handle_client(client_socket, address):
         elif cmd == "get":
             send_files(received, client_socket)
         elif cmd == "stop":
+            # Quando o cliente sinaliza o encerramento, fechamos o socket.
             client_socket.close()
             break
         else:
@@ -136,7 +153,7 @@ def handle_client(client_socket, address):
 
 
 def main():
-    # Criando socket
+    # Criamos o socket do servidor
     try:
         s = socket.socket()
         s.bind((SERVER_HOST, SERVER_PORT))
@@ -145,12 +162,15 @@ def main():
         print(str(err))
         exit()
 
+    # Passamos a escutar o socket criado
     s.listen(5)
-    print(f"[*] Escutando em {SERVER_HOST}:{SERVER_PORT}")
+    print(f"Escutando em {SERVER_HOST}:{SERVER_PORT}")
 
+    # Executamos o servidor 'infinitamente'
     while True:
+        # A cada conexão aceita, criamos uma nova thread
         client_socket, address = s.accept()
-        print(f"[+] {address} is connected.")
+        print(f"{address} conectou-se.")
         start_new_thread(handle_client, (client_socket, address))
 
 
